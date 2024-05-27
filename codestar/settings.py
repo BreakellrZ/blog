7b1cf43667a297139df9bb5bee48978d7e586ffa
Colin Breakell <cbreakell99@gmail.com>
2024-05-27 18:34:15 +0000

    Added some tags and variables to index.html and base.html - added post.excerpt for index.htmk page

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '8000-breakellrz-blog-blwoj5e3cwc.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '8000-breakellrz-blog-blwoj5e3cwc.ws-eu111.gitpod.io', '127.0.0.1']
 
 
 # Application definition
